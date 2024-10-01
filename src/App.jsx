892d824d354500cce3d370600b545a57688771f1
siriwatknp <siriwatkunaporn@gmail.com>
2024-10-01 10:23:10 +0700

    first error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+function Title({ text, ms }) {
+  return (
+    <title
+      style={{
+        margin: "auto",
+        fontSize: 100,
+        textAlign: "center",
+        marginTop: "12vh",
+      }}
+    >
+      {text.length ? text.split("").map((fragment) => <b>{fragment}</b>) : ""}{" "}
+      <br />
+      {ms.toFixed(2)} sec
+    </title>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
+  React.useEffect(async () => {
+    setTimeout(() => {
+      setCounter(counter + 1);
+    }, 1000);
+  }, [counter]);
 
-  return <div style={{ maxWidth: 400, margin: "auto" }}>Hello</div>;
+  return (
+    <text>
+      <Title test="You won! 👏" ms={`${countr}`} />
+    </text>
+  );
 }
 
 export default App;
